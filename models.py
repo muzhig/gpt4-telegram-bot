b03a86166791744dd36a2b9640cc9d232333741f
Arseniy Potapov <arseniy@potapov.dev>
2023-03-26 20:21:28 +0600

    Pynamo model

---
+++ models.py
@@ -0,0 +1,18 @@
+from pynamodb.attributes import UnicodeAttribute, NumberAttribute, JSONAttribute, ListAttribute
+from pynamodb.models import Model
+
+
+class Messages(Model):
+    class Meta:
+        table_name = "gpt4-tg-bot-messages"
+    id = UnicodeAttribute(hash_key=True)  # email or phone number
+    reply_to = UnicodeAttribute(null=True)
+    thread_id = UnicodeAttribute(null=True)
+    messages_in_thread = ListAttribute()
+    chat_id = UnicodeAttribute()
+    created = NumberAttribute()
+    expires = NumberAttribute()  # + 14 days
+    message = UnicodeAttribute()
+    sender = UnicodeAttribute()
+    meta = JSONAttribute()
+
